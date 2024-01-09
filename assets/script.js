2af2da31ecd71e0dde80d4696cac192bb9ad8d79
Mandy Miller <mandy@cullenmiller.com>
2024-01-09 15:13:48 +0000

    Completed js code comments, and final formatting of all code files. Attempted to restructure arrays but running out of time, so committing with a working version

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,51 @@
 // Function to generate a random password based on criteria
 function generatePassword() {
+  // Define the arrays for the accepted character types
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz"; //Have removed comma separation as was affecting the output of pw generated - was including the commas. There's Probably a better way to do it!
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numericChars = "0123456789";
+  const specialChars = "$@%&*£";
 
-// Define the arrays for the accepted character types
-const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numericChars = "0123456789";
-const specialChars = "$@%&*£";
+  let password = "";
+  const allChars = []; // allChars Empty array which is populated based on the user choice of lowercase / uppercase etc
 
-let password = "";
-const allChars = [];  // allChars Empty array which is populated based on the user choice of lowercase / uppercase etc
+  const length = parseInt(
+    prompt(
+      "Please enter the length of the password (between 8 and 128 characters):"
+    )
+  ); // Prompt user for password options
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter a valid password length between 8 and 128 characters."); // Validates that pw is correct length, if not, prompts user to enter again
+    return;
+  }
 
-const length = parseInt(prompt("Please enter the length of the password (between 8 and 128 characters):")); // Prompt user for password options
-if (isNaN(length) || length < 8 || length > 128) {
-alert("Please enter a valid password length between 8 and 128 characters.");   // Validates that pw is correct length
-return;
-}
+  if (confirm("Would you like to include lowercase characters? Click OK")) {
+    allChars.push(lowercaseChars); // If selected, lowercase is added to allChars array
+  }
+  if (confirm("Would you like to include uppercase characters? Click OK")) {
+    allChars.push(uppercaseChars); // If selected, uppercase is added to allChars array
+  }
+  if (confirm("Would you like to include numeric characters? Click OK")) {
+    allChars.push(numericChars); // If selected, numeric is added to allChars array
+  }
+  if (confirm("Would you like to include special characters? Click OK")) {
+    allChars.push(specialChars); // If selected, special is added to allChars array
+  }
 
-if (confirm("Would you like to include lowercase characters? Click OK")) { 
-allChars.push(lowercaseChars);      // If selected, lowercase is added to allChars array
-}
-if (confirm("Would you like to include uppercase characters? Click OK")) { 
-allChars.push(uppercaseChars);      // If selected, uppercase is added to allChars array
-}
-if (confirm("Would you like to include numeric characters? Click OK")) { 
-allChars.push(numericChars);        // If selected, numeric is added to allChars array
-}
-if (confirm("Would you like to include special characters? Click OK")) {
-allChars.push(specialChars);      // If selected, special is added to allChars array
-}
+  if (allChars.length === 0) {
+    alert("You must select at least one character type."); // Validates that pw contains at least 1 character type
+    return;
+  }
 
-if (allChars.length === 0) {
-alert("You must select at least one character type."); // Validates that pw contains at least 1 character type
-return;
-}
+  const combinedChars = allChars.join(",");    // creates a comma separated array from the chanracter type constants delcared in lines 4-7 ...
+  const combinedArray = combinedChars.split("");    // .. i feel this is clumsy but it works!
 
-const combinedChars = allChars.join(",");
-const combinedArray = combinedChars.split("");
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * combinedArray.length); // Selecting random elements from the allChars array. Lenght of array is defined by user input from Ln13
+    password += combinedArray[randomIndex];
+  }
 
-for (let i = 0; i < length; i++) {
-const randomIndex = Math.floor(Math.random() * combinedArray.length); // Selecting random elements from the allChars array. Lenght of array is defined by user input from Ln13 
-password += combinedArray[randomIndex]; 
-}
-
-    document.getElementById("password").textContent = password;
+  document.getElementById("password").textContent = password;
 }
 
 // Add click event listener to the "Generate Password" button
